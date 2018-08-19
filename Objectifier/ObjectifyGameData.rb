d219b6a81579fc79fe22a19d61dea77129cb0b99
Eric Hobsbawm <hobsmod@gmail.com>
2018-08-19 15:43:14 -0700

    this is mostly to test the game data parsers

--- Objectifier/ObjectifyGameData.rb
+++ Objectifier/ObjectifyGameData.rb
@@ -5,6 +5,7 @@ require 'yaml'
 require_relative '..\Methods\loadPoptypes.rb'
 require_relative '..\Methods\loadProdtypes.rb'
 require_relative '..\Methods\loadTech.rb'
+require_relative '..\Methods\loadInventions.rb'
 
 mod_dir = 'C:\Program Files (x86)\Steam\steamapps\common\Victoria 2'
 write_dir = 'C:\Program Files (x86)\Steam\steamapps\common\Victoria 2\mod\VictoriaII-Save-Parser\Mod Data\Vanilla'
@@ -12,8 +13,10 @@ write_dir = 'C:\Program Files (x86)\Steam\steamapps\common\Victoria 2\mod\Victor
 poptypes = loadPoptypes(mod_dir)
 prod_types = loadProdtypes(mod_dir)
 tech = loadTech(mod_dir)
+invent = loadInventions(mod_dir)
 
 
 File.write(write_dir + '\\Poptypes.yaml', poptypes.to_yaml)
 File.write(write_dir + '\\production_types.yaml', prod_types.to_yaml)
 File.write(write_dir + '\\technologies.yaml', tech.to_yaml)
+File.write(write_dir + '\\inventions.yaml', invent.to_yaml)
